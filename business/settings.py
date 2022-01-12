3e59db3d42fdc92524ee0fcda773b309326496ee
Harsh Modi <harshmodibtp30@gmail.com>
2022-01-12 14:10:57 +0530

    fixed

--- business/settings.py
+++ business/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["classifiyo.herokuapp.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["classifiyo.herokuapp.com","localhost","127.0.0.1:8000"]
 
 
 # Application definition
